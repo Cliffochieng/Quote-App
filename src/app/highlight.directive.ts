ae5a0ea32ab53f4a9d0f868b95efa316a9b3e3be
Cliffochieng <cliffordochieng844@gmail.com>
2020-09-27 15:01:47 +0300

    add quote-form,quote-details and quotes

---
+++ src/app/highlight.directive.ts
@@ -0,0 +1,25 @@
+import { Directive ,ElementRef,HostListener} from '@angular/core';
+
+@Directive({
+  selector: '[appHighlight]'
+})
+export class HighlightDirective {
+
+  constructor(private elem:ElementRef) {}
+  
+  @HostListener("click") onClicks(){
+    this.textDeco("blue")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("transparent")
+  }
+
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.backgroundColor=action;
+
+  }
+
+
+}
---
+++ src/app/quote.ts
@@ -0,0 +1,10 @@
+export class Quote {
+  public upVote: true;
+  showCompleteDate: boolean;
+  constructor(public title: string, public body: string, public author: string, public completeDate: Date, public like: number,
+    public dislike: number) {
+    this.showCompleteDate = false;
+  }
+
+
+}
