649d308215788983aea522061991c9326b7dc694
Cliffochieng <cliffordochieng844@gmail.com>
2020-09-27 15:38:41 +0300

    add wisdom quote

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -12,7 +12,7 @@ export class QuoteComponent implements OnInit {
  new Quote('Success', 'Nothing is impossible the word itself says im possible.', 'Albert Einstein', new Date(2020, 9 - 1, 27),0,0),
   new Quote('Failure ', "Anyone who has never made a mistake has never tried anything new.", 'Audrey Helpburn', new Date(2020, 9 - 1, 27),0,0),
   new Quote('Fear', 'Life shrinks or expands in proportion to ones courage', 'Anais Ninpher', new Date(2020, 9 - 1, 27),0,0),
-  new Quote('Wisdom', 'Knowing yourself is the beginning of all wisdom.', 'William Shakespear', new Date(2020, 8 - 1, 27),0,0),
+  new Quote('Wisdom', 'A wiseman never knows all,only fools know everything', 'William Shakespear', new Date(2020, 8 - 1, 27),0,0),
   new Quote('Passion ', 'Passion is the genesis of genius.', 'Oprah Winfrey', new Date(2020, 9 - 1, 27),0,0),
   new Quote('Investment ', 'Never invest in a business you cannot understand.', 'Benjamin Franklin', new Date(2020, 9 - 1, 27),0,0)
 ];
